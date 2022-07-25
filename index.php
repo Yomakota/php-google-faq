75d4cd58fbd071ba9db5346271c2fe9597b76fe1
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-25 17:44:43 +0200

    add main

--- index.php
+++ index.php
@@ -21,14 +21,14 @@ $faq = [
     [
         'question' => "Perché il mio account è associato a un paese?",
         'answer' => "<p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
-            <ul>
+            <ol>
                 <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                     <ol type='a'>
                     <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                     <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
                     </ol>
                 <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
-            </ul>
+            </ol>
          <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
         <h3>Stabilire il paese associato al tuo account</h3>
         <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p>
@@ -75,3 +75,14 @@ $faq = [
         </div>
     </header>
     <!-- /header -->
+    <!-- main -->
+    <main>
+        <div class="container">
+            <?php
+            foreach ($faq as $post) { ?>
+                <h2><?= $post['question']; ?></h2>
+                <div><?= $post['answer'] ?></div>
+            <?php } ?>
+        </div>
+    </main>
+    <!-- /main -->
